d58d51c6607dd426ee2464c221c6f2f614d969a6
jhonatan2104 <xdjhonatan0@gmail.com>
2021-02-11 14:25:54 -0300

    feat: init micro service medicine

---
+++ medicine/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  roots: [
+    '<rootDir>/src'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
